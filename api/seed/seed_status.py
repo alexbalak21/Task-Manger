2f1a3aa29f340eda22a658465627c9a39014b0a1
alexbalak21 <alex.balak@outlook.com>
2026-03-16 23:07:52 +0100

    created seed for priority & status

---
+++ api/seed/seed_status.py
@@ -0,0 +1,13 @@
+from model.Status import Status
+from extensions.db import db
+
+def seed_status():
+    statuses = ["Pending", "Awaiting", "In Progress", "On Hold", "Completed", "Cancelled"]
+
+    for name in statuses:
+        existing = Status.query.filter_by(name=name).first()
+        if not existing:
+            db.session.add(Status(name=name))
+            print(f"Seeded status: {name}")
+
+    db.session.commit()
