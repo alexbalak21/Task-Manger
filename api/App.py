c6880370aec477cc7a29401e4f50ff4f07f5a657
alexbalak21 <alex.balak@outlook.com>
2026-03-16 21:51:37 +0100

    creating the api

---
+++ api/App.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from controller.HomeController import home_blueprint
+
+def create_app():
+    app = Flask(__name__)
+
+    # Register blueprints
+    app.register_blueprint(home_blueprint)
+
+    return app
